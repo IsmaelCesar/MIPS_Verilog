262e2e4e3dc82a5fb219c545cd1b11582d2d3840
IsmaelCesar <leamscesar@gmail.com>
2018-07-04 22:51:14 -0300

    Adcionando primeiras alteracoes para JAL

--- CONTROL.v
+++ CONTROL.v
@@ -33,6 +33,8 @@
 `define OPCODE_LBU    6'b100100
 `define OPCODE_LUI    6'b001111
 
+`define OPCODE_JAL 	 6'b000011
+
 `define ALUOP_ADDI    6'b000000
 `define ALUOP_LW      6'b000000
 `define ALUOP_SW      6'b000000
@@ -73,7 +75,7 @@ reg branch;
 reg read_mem;
 reg write_mem;
 reg write_reg;
-reg  mux_write_rt_rd;
+reg [1:0]mux_write_rt_rd;
 reg mux_alu_src_reg_imm;
 reg [5:0] alu_op;
 reg mux_branch_jump;
@@ -253,6 +255,20 @@ always @(nrst, opcode) begin : decode_thread
       mux_pc_branch 		= 0;
       mux_reg_src_alu_mem 	= 0;  
     end
+	 
+	 `OPCODE_JAL: begin
+		branch 			= 0;
+      read_mem 			= 0;
+      write_mem 		= 0;
+      write_reg 		= 0;
+      alu_op 			= `ALUOP_TIPO_R;
+      mux_write_rt_rd 		= 2;
+      mux_alu_src_reg_imm 	= 0;
+      mux_branch_jump 		= 0;
+      mux_pc_branch 		= 0;
+      mux_reg_src_alu_mem 	= 0;
+	 end
+	 
     endcase
   end
 end
---
+++ MUX31.v
@@ -0,0 +1,40 @@
+/**
+ * Universidade Federal Rural de Pernambuco
+ * Departamento de Estat�stica e Inform�tica
+ * Disciplina: Arquitetura e Organiza��o de Computadores
+ * 
+ * Mux 3 para 1.
+ *
+ * @author Ismael Cesar (ismael.cesar@ufrpe.br)
+ */
+ 
+ 
+ 
+ `define RETURN_ADDRES_REG_NUMBER 32'h1F //31 -> Endereço de RA no MIPS reference data
+ 
+ /*Sinal que entra no MUX é modificado
+	Caso a instrução seja  jal, O mux manda o RA como registrador destino
+ */
+ 
+module MUX31(A,
+				 B,
+				 O,
+				 S);
+
+parameter DATA_WIDTH = 32;
+parameter SIGNAL_WIDTH = 2;
+
+input A;
+input B;
+input S;
+output O;
+
+wire [DATA_WIDTH-1:0] A;
+wire [DATA_WIDTH-1:0] B;
+reg  [DATA_WIDTH-1:0] O;
+wire [SIGNAL_WIDTH-1:0]S;
+
+assign O = (S == 2'h0)? A: (S == 2'h1)? B : RETURN_ADDRES_REG_NUMBER;
+
+
+endmodule
