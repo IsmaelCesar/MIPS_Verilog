a8e21c84dffb5f44c31da2b99d214971ed05eb08
IsmaelCesar <leamscesar@gmail.com>
2018-07-25 09:38:32 -0300

    Sequencia de instruções
    Sequencia de instruções para serem usadas em testes

--- instrucoes.asm
+++ instrucoes.asm
@@ -1,13 +1,15 @@
 .globl main
 main:
 addi $t0, $zero, 1 
-addi $t1, $zero, 2 
-
-jal other
-
-sub $t0, $t0,$t2
-
-other:
-addi $t2, $zero, 3 
-addi $t3, $zero, 4 
-jr $ra 
+addi $t1, $zero, 1
+addi $s0, $zero, -5
+and  $t2, $t0,$t1
+or   $t3, $zero,$t0
+xor  $t4, $t0,$t1
+andi  $t5, $t0,1
+ori   $t6, $zero,1
+xori  $t4, $t0, 1
+slti  $t0, $t1, 3
+addu  $t1, $t1,$s0
+addiu $t1, $t1,-7
+sltiu  $t0, $s0,-6
