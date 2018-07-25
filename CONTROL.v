cbae8ded519d9ed9115839882d220e2b4509f10c
IsmaelCesar <leamscesar@gmail.com>
2018-07-25 09:26:55 -0300

    Sinais de controle Addiu
    Adicionando sinais de controle utilizados pela operação ADDIU

--- CONTROL.v
+++ CONTROL.v
@@ -151,6 +151,21 @@ always @(nrst, opcode) begin : decode_thread
 		mux_j_type_addr_to_write = 1;
 		mux_j_type_addr_to_read =  1;
     end
+	 
+	 `OPCODE_ADDIU: begin
+      branch 			= 0;
+      read_mem 			= 0;
+      write_mem 		= 0;
+      write_reg 		= 1;
+      alu_op 			= `OPCODE_ADDIU;
+      mux_write_rt_rd 		= 0;
+      mux_alu_src_reg_imm 	= 1;
+      mux_branch_jump 		= 1;
+      mux_pc_branch 		= 0;
+      mux_reg_src_alu_mem 	= 1;  
+		mux_j_type_addr_to_write = 1;
+		mux_j_type_addr_to_read =  1;
+    end
  
     `OPCODE_LW: begin
       branch 			= 0;
